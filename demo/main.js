37901af3d4a5ab37ccc049a6e754f442a9bd62d8
Bogdan <Baga13511@yandex.ru>
2025-12-16 15:06:46 +0300

    add Russian Language

--- demo/main.js
+++ demo/main.js
@@ -5,7 +5,16 @@ let keyboard = new Keyboard({
   onChange: input => onChange(input),
   onKeyPress: button => onKeyPress(button),
   useMouseEvents: true,
-  modules: [swipe]
+  modules: [swipe],
+  layout: {
+    default: [
+      "ё 1 2 3 4 5 6 7 8 9 0 - =",
+      "й ц у к е н г ш щ з х ъ",
+      "ф ы в а п р о л д ж э",
+      "{shift} я ч с м и т ь б ю .",
+      "{space}"
+    ]
+  }
 });
 
 console.log(keyboard);
