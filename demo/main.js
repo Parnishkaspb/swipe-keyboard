092d4fa0da215b5f7c00dc7c1f5eb6e1f8c1e099
Bogdan <Baga13511@yandex.ru>
2025-12-16 15:11:13 +0300

    Edit keyboard to simple

--- demo/main.js
+++ demo/main.js
@@ -8,11 +8,10 @@ let keyboard = new Keyboard({
   modules: [swipe],
   layout: {
     default: [
-      "ё 1 2 3 4 5 6 7 8 9 0 - =",
-      "й ц у к е н г ш щ з х ъ",
+      "й ц у к е н г ш щ з х",
       "ф ы в а п р о л д ж э",
-      "{shift} я ч с м и т ь б ю .",
-      "{space}"
+      "{shift} я ч с м и т ь б ю {backspace}",
+      "{capslock} {globe} , {space} . {enter}"
     ]
   }
 });
